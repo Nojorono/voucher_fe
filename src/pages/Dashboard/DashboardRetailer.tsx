29fa75d6fa7a2cb4e677393a74ed235d8896644f
dmsrsd <dimasrosadi9029@gmail.com>
2025-01-23 16:21:58 +0700

    Dummy Table Dashboard

--- src/pages/Dashboard/DashboardRetailer.tsx
+++ src/pages/Dashboard/DashboardRetailer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TableOne from '../../components/Tables/TableOne';
+import ChartThree from '../../components/Charts/ChartThree';
 import DataTableDashboard from '../../components/Tables/DataTableDashboard'
 
 
@@ -8,6 +9,8 @@ const DashboardRetailer: React.FC = () => {
     <>
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
         <div className="col-span-12">
+          {/* <TableOne /> */}
+          {/* <ChartThree /> */}
           <DataTableDashboard />
         </div>
       </div>
--- src/pages/Dashboard/DashboardVoucher.tsx
+++ src/pages/Dashboard/DashboardVoucher.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import DataTableVoucher from '../../components/Tables/DataTableVoucher'
+import TableOne from '../../components/Tables/TableOne';
+import ChartThree from '../../components/Charts/ChartThree';
+
+
 
 function DashboardVoucher() {
-    return (
-        <>
-        <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-          <div className="col-span-12">
-            <DataTableVoucher />
-          </div>
+  return (
+    <>
+      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+        <div className="col-span-12">
+          {/* <ChartThree /> */}
+          {/* <TableOne /> */}
+          <DataTableVoucher />
         </div>
-      </>
-    )
+      </div>
+    </>
+  )
 }
 
 export default DashboardVoucher
