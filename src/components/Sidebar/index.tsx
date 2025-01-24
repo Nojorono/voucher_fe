536db7fc887b32f5a04f4532a596ff34f45fca5e
dmsrsd <dimasrosadi9029@gmail.com>
2025-01-24 08:58:02 +0700

    fixing sign in hide/show password

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -73,9 +73,9 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
       className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0 ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'}`}
     >
       <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
-        <NavLink to="/profile">
+        {/* <NavLink to="/profile">
           <img src={Logo} alt="Logo" />
-        </NavLink>
+        </NavLink> */}
 
         <button
           ref={trigger}
