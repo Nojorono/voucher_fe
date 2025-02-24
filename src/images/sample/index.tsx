ae9a7a344b57217d01f044d9d373fed26161ab15
dmsrsd <dimasrosadi9029@gmail.com>
2025-02-24 10:54:42 +0700

    fixing BG img

--- src/images/sample/index.tsx
+++ src/images/sample/index.tsx
@@ -2,6 +2,9 @@ import sample1 from './sample-1.jpeg';
 import sample2 from './sample-2.jpeg';
 import sample3 from './sample-3.jpeg';
 import BG from './BG.jpeg';
+import BG3 from './BG-3.jpg';
 
 
-export { sample1, sample2, sample3, BG };
+
+
+export { sample1, sample2, sample3, BG, BG3 };
