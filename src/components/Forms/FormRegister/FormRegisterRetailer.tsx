a0c214c6ad51e98a0afe686a22f38f816db0a9e8
dmsrsd <dimasrosadi9029@gmail.com>
2025-03-12 07:42:36 +0700

    fixing upload file

--- src/components/Forms/FormRegister/FormRegisterRetailer.tsx
+++ src/components/Forms/FormRegister/FormRegisterRetailer.tsx
@@ -4,8 +4,9 @@ import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 import { stagingURL } from '../../../utils/API'
 import Select from 'react-select';
-import { sample1, sample2, sample3 } from '../../../images/sample/index';
+import { sample1, sample2, sample3, noImg } from '../../../images/sample/index';
 import CustomToast, { showErrorToast, showSuccessToast } from '../../../components/Toast/CustomToast';
+import { Webcam } from '@webcam/react';
 
 interface FormProps<T extends FieldValues> {
     onSubmit: SubmitHandler<T>;
@@ -283,8 +284,75 @@ const FormRetailerRegister = <T extends FieldValues>({ onSubmit, fields }: FormP
                                             accept="image/*"
                                             capture="environment"
                                             {...register(`${String(field.name)}_${index}` as any, { required: field.required, onChange: handleFileChange })}
-                                            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                            className="w-32 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                                         />
+
+                                        {/* WITH OPEN CAMERA */}
+                                        {/* <button
+                                            type="button"
+                                            onClick={async () => {
+                                                try {
+                                                    const webcamElement = document.getElementById(`webcam-container-${index}`);
+                                                    if (webcamElement) {
+                                                        webcamElement.style.display = 'block';
+                                                    }
+                                                } catch (error) {
+                                                    alert('Permission to access camera was denied.');
+                                                }
+                                            }}
+                                            className="w-auto bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition-colors mt-2"
+                                            id={`open-camera-button-${index}`}
+                                        >
+                                            Buka Camera
+                                        </button>
+
+                                        <div id={`webcam-container-${index}`} style={{ display: 'none' }} className='mt-2'>
+                                            <Webcam mirrored>
+                                                {({ getSnapshot }) => (
+                                                    <div>
+                                                        <button
+                                                            type="button"
+                                                            onClick={() => {
+                                                                const snapshot = getSnapshot({ quality: 0.8 });
+                                                                if (snapshot) {
+                                                                    const base64Data = snapshot.split(',')[1];
+                                                                    const byteCharacters = atob(base64Data);
+                                                                    const byteNumbers = new Array(byteCharacters.length);
+                                                                    for (let i = 0; i < byteCharacters.length; i++) {
+                                                                        byteNumbers[i] = byteCharacters.charCodeAt(i);
+                                                                    }
+                                                                    const byteArray = new Uint8Array(byteNumbers);
+                                                                    const file = new File([byteArray], `photo_${index}.jpg`, { type: 'image/jpeg' });
+
+                                                                    handleFileChange({
+                                                                        target: { files: [file] }
+                                                                    } as unknown as React.ChangeEvent<HTMLInputElement>);
+
+                                                                    // Display the captured photo
+                                                                    const objectUrl = URL.createObjectURL(file);
+                                                                    const imgElement = document.getElementById(`captured-photo-${index}`) as HTMLImageElement;
+                                                                    if (imgElement) {
+                                                                        imgElement.src = objectUrl;
+                                                                    }
+
+                                                                    // Hide webcam and show "Buka Camera" button again
+                                                                    const webcamElement = document.getElementById(`webcam-container-${index}`);
+                                                                    const openCameraButton = document.getElementById(`open-camera-button-${index}`);
+                                                                    if (webcamElement && openCameraButton) {
+                                                                        webcamElement.style.display = 'none';
+                                                                        openCameraButton.style.display = 'block';
+                                                                    }
+                                                                }
+                                                            }}
+                                                            className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition-colors mt-3"
+                                                        >
+                                                            Ambil Foto
+                                                        </button>
+                                                    </div>
+                                                )}
+                                            </Webcam>
+                                        </div>
+                                        <img src={noImg} id={`captured-photo-${index}`} alt={`Captured ${index + 1}`} className="mt-3 w-32 h-32 object-cover" /> */}
                                     </div>
                                 ))}
                             </div>
