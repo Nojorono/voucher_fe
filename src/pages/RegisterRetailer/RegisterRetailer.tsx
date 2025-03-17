c50049c9b0bdecf2f29dc265d7a4e9fddb3fd101
dmsrsd <dimasrosadi9029@gmail.com>
2025-03-17 08:43:49 +0700

    fixing upload file

--- src/pages/RegisterRetailer/RegisterRetailer.tsx
+++ src/pages/RegisterRetailer/RegisterRetailer.tsx
@@ -116,15 +116,15 @@ const RegisterRetailer: React.FC = () => {
         } catch (error) {
             console.error(`Error Log: ${(error as Error).message}`);
             setLoading(false);
-            setTimeout(() => {
-                const errorMessage = (error as Error).message;
-                if (errorMessage.includes('Unexpected end of input')) {
-                    showSuccessToast('Retailer registered successfully.');
-                } else {
-                    showErrorToast(`Error: ${errorMessage}`);
-                }
-            }, 1000);
-            throw error;
+            // setTimeout(() => {
+            //     const errorMessage = (error as Error).message;
+            //     if (errorMessage.includes('Unexpected end of input')) {
+            //         showSuccessToast('Retailer registered successfully.');
+            //     } else {
+            //         showErrorToast(`Error: ${errorMessage}`);
+            //     }
+            // }, 1000);
+            // throw error;
         }
     };
 
