953d24a3884730f5bb8bcb52ba03eab919c5295d
dmsrsd <dimasrosadi9029@gmail.com>
2025-03-25 10:11:07 +0700

    fixing redirect register

--- src/components/Forms/FormRegister/FormRegisterRetailer.tsx
+++ src/components/Forms/FormRegister/FormRegisterRetailer.tsx
@@ -4,9 +4,8 @@ import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 import { stagingURL } from '../../../utils/API'
 import Select from 'react-select';
-import { sample1, sample2, sample3, noImg } from '../../../images/sample/index';
-import CustomToast, { showErrorToast, showSuccessToast } from '../../../components/Toast/CustomToast';
-import { Webcam } from '@webcam/react';
+import { sample1, sample2, sample3 } from '../../../images/sample/index';
+import CustomToast, { showErrorToast } from '../../../components/Toast/CustomToast';
 
 interface FormProps<T extends FieldValues> {
     onSubmit: SubmitHandler<T>;
@@ -363,18 +362,13 @@ const FormRetailerRegister = <T extends FieldValues>({ onSubmit, fields }: FormP
                     </div>
                 ))}
 
-
                 <div className="col-span-1 md:col-span-2">
-                    <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-2" role="alert">
-                        <strong className="font-bold">Peringatan!</strong>
+                    <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-5">
+                        <strong className="font-bold">Peringatan!&nbsp;</strong>
                         <span className="block sm:inline">
-                            Kode Voucher hanya akan dikirimkan setelah pihak Admin sudah melakukan verifikasi dan akan dikirimkan melalui WhatsApp dari nomor <b>081220199495</b> saja.
+                            Voucher akan dikirimkan oleh pihak admin hanya melalui no Whatsapp <b>081220199495</b> setelah melalui proses verifikasi.
                         </span>
                         <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
-                            <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                <title>Close</title>
-                                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"></path>
-                            </svg>
                         </span>
                     </div>
 
