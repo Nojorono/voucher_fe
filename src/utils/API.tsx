7cb9f997c335f0e5424571c91f3300e2b2da502a
djination <b.senjana@gmail.com>
2025-07-10 15:12:43 +0700

    updated

--- src/utils/API.tsx
+++ src/utils/API.tsx
@@ -1,5 +1,5 @@
-const localURL = 'http://localhost:8081';
-const stagingURL = 'http://localhost:8081';
+const localURL = 'http://localhost:8080';
+const stagingURL = 'http://localhost:8080';
 // const stagingURL = 'http://10.0.29.49:8081';
 // const s3URL = 'https://s3-ap-southeast-1.amazonaws.com/retailer-verification';
 
