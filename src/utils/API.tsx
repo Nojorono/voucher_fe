2d4d1248a304a047bd763683c43848d2d16674bc
djination <b.senjana@gmail.com>
2025-07-11 09:12:39 +0700

    updated for ALB port

--- src/utils/API.tsx
+++ src/utils/API.tsx
@@ -1,6 +1,6 @@
-const localURL = 'http://localhost:8081';
-const stagingURL = 'http://localhost:8081';
-// const stagingURL = 'http://10.0.29.49:8081';
+const localURL = 'http://your-alb-dns-name.us-east-1.elb.amazonaws.com:8082';
+const stagingURL = 'http://your-alb-dns-name.us-east-1.elb.amazonaws.com:8082';
+// For development: 'http://localhost:8082'
 // const s3URL = 'https://s3-ap-southeast-1.amazonaws.com/retailer-verification';
 
 export { localURL, stagingURL };
